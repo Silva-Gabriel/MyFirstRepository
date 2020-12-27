a950a8ea31e54076ac1df8da9d8e02504360e2f6
soldierGS <soldierGS@github.com>
2020-12-27 20:05:52 -0300

    Programa1-Python
    O primeiro programa é sempre o mais emocionante,e que tal começar com algo básico,como um hello world.

---
+++ ProjetosTreino-Python/Programa 1.py
@@ -0,0 +1,4 @@
+#Aquele programa que não pode faltar,sempre que um estudante começa a aprender uma nova tecnologia
+print("\033[035m=-==-=-=-=-=-=")
+print("\033[034mHELLO,WORLD!!!")
+print("\033[035m=-==-=-=-=-=-=")
