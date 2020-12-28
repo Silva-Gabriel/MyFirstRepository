e894a6ae81ca11fa3ebba776bc04298c3558a3a5
soldierGS <soldierGS@github.com>
2020-12-27 21:23:52 -0300

    Leitura inline-Python
    Leituras inline são muitos ultilizadas em maratonas de programação e em sites como URI Judge,este é um exemplo de como pode ser feito.

---
+++ ProjetosTreino-Python/Variaveis inline.py
@@ -0,0 +1,14 @@
+#Para fazer com que leia na mesma linha você somente precisará fazer uma lista vazia recebendo um input.split(" ")
+lista = []
+lista = input().split(" ")
+#A cada espaço será alocada 1 variável,como temos 3 neste exemplo,você poderá escrever um nome,ou colocar números,separando-os por espaço
+A = lista[0]
+B = lista[1]
+C = lista[2]
+#primeira variável
+print("1:",A)
+#segunda
+print("2:",B)
+#terceira
+print("3:",C)
+##Isso é bastante usado para concursos de programação e em sites como URI Judge em que a maioria dos exercícios são feitas entradas inline
